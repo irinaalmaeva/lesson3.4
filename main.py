6b596ed0c01e2f5d1f50454b66f457e9d183a787
irinaalmaeva <almaeva.irina@gmail.com>
2024-06-04 22:30:48 +0300

    add code main

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 
+import random
 
+students = ['Андрей', 'Анна', 'Артём', 'Антонина', 'Богдан', 'Владимир', 'Вероника', 'Геннадий', 'Дмитрий', 'Евгений',
+            'Елена', 'Игорь', 'Ирина', 'Кирилл', 'Константин', 'Лев', 'Максим', 'Мария', 'Михаил', 'Наталья', 'Никита',
+            'Олег', 'Ольга', ]
+
+selected_students = random.sample(students, 5)
+print("имена студентов, которые будут отвечать на уроке:")
+
+for student in selected_students:
+    print(student)
+
+for student in selected_students:
+    students.remove(student)
