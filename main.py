745d4fc468f1a8dfff3b02558dd7ffd429f493a6
irinaalmaeva <almaeva.irina@gmail.com>
2024-06-05 22:04:14 +0300

    changed code select students in new-branch

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 
 import random
 
-students = ['Алексей', 'Анна', 'Артём', 'Антонина', 'Борис', 'Владимир', 'Вероника', 'Григорий', 'Дмитрий', 'Евгений',
-            'Елена', 'Игорь', 'Ирина', 'Кирилл', 'Константин', 'Лев', 'Максим', 'Мария', 'Михаил', 'Наталья', 'Никита',
-            'Олег', 'Ольга', 'Федор']
+students = {
+    'male': ['Алексей', 'Артём', 'Борис', 'Владимир', 'Григорий', 'Дмитрий', 'Евгений', 'Игорь', 'Кирилл', 'Константин', 'Лев', 'Максим', 'Михаил', 'Никита', 'Олег', 'Федор'],
+    'female': ['Анна', 'Антонина', 'Вероника', 'Елена', 'Ирина', 'Мария', 'Наталья', 'Ольга']
+}
 
-selected_students = random.sample(students, 4)
+selected_students = random.sample(students['male'], 2) + random.sample(students['female'], 2)
 print("имена студентов, которые будут отвечать на уроке:")
 
 for student in selected_students:
     print(student)
 
 for student in selected_students:
-    students.remove(student)
+    if student in students['male']:
+        students['male'].remove(student)
+    elif student in students['female']:
+        students['female'].remove(student)
 
 print("\nоставшиеся студенты после отбора:")
-for student in students:
-    print(student)
+for gender in students:
+    for student in students[gender]:
+        print(student)
 
 
