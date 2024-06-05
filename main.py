c0f91416c2b8030a28cd10cecc7f96dfdf488ed8
irinaalmaeva <almaeva.irina@gmail.com>
2024-06-05 22:21:38 +0300

    delet code except print in main

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
 print("Я попробую решить конфликт")
 
-import random
-
-students = ['Алексей', 'Анна', 'Артём', 'Антонина', 'Борис', 'Владимир', 'Вероника', 'Григорий', 'Дмитрий', 'Евгений',
-            'Елена', 'Игорь', 'Ирина', 'Кирилл', 'Константин', 'Лев', 'Максим', 'Мария', 'Михаил', 'Наталья', 'Никита',
-            'Олег', 'Ольга', 'Федор']
-
-selected_students = random.sample(students, 4)
-print("имена студентов, которые будут отвечать на уроке:")
-
-for student in selected_students:
-    print(student)
-
-for student in selected_students:
-    students.remove(student)
